ba8e6dafa92b316ce2f8d02dec9691a1817f7e8b
felipemeriga <felipe.meriga@gmail.com>
2021-04-20 11:30:14 -0300

    Adding a test for the insert functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package golang_binary_search_tree
 
 import (
 	"github.com/cheekybits/genny/generic"
+	"sync"
 )
 
 // Item the type of the binary search tree
@@ -13,4 +14,37 @@ type Node struct {
 	value Item
 	left  *Node //left
 	right *Node //right
+}
+
+// ItemBinarySearchTree the binary search tree of Items
+type ItemBinarySearchTree struct {
+	root *Node
+	lock sync.RWMutex
+}
+
+// Insert inserts the Item t in the tree
+func (bst *ItemBinarySearchTree) Insert(key int, value Item) {
+	n := &Node{key, value, nil, nil}
+	if bst.root == nil {
+		bst.root = n
+	} else {
+		insertNode(bst.root, n)
+	}
+}
+
+// internal function to find the correct place for a node in a tree
+func insertNode(node, newNode *Node) {
+	if newNode.key < node.key {
+		if node.left == nil {
+			node.left = newNode
+		} else {
+			insertNode(node.left, newNode)
+		}
+	} else {
+		if node.right == nil {
+			node.right = newNode
+		} else {
+			insertNode(node.right, newNode)
+		}
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package golang_binary_search_tree
+
+import (
+	"testing"
+)
+
+var bst ItemBinarySearchTree
+
+
+func fillTree(bst *ItemBinarySearchTree) {
+	bst.Insert(8, "8")
+	bst.Insert(4, "4")
+	bst.Insert(10, "10")
+	bst.Insert(2, "2")
+	bst.Insert(6, "6")
+	bst.Insert(1, "1")
+	bst.Insert(3, "3")
+	bst.Insert(5, "5")
+	bst.Insert(7, "7")
+	bst.Insert(9, "9")
+}
+
+func TestInsert(t *testing.T) {
+	fillTree(&bst)
+
+	bst.Insert(11, "11")
+}
